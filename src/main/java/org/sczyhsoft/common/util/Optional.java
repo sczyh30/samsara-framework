bcc8b75a3b86b431e91d9ded1a1803c7b476949c
sczyh30 <791801973@qq.com>
2015-07-07 15:18:32 +0800

    a little update

---
+++ src/main/java/org/sczyhsoft/common/util/Optional.java
@@ -0,0 +1,25 @@
+package org.sczyhsoft.common.util;
+
+/**
+ * ScZyhSoft Common Library
+ * Optional Container
+ * 此容器可以存储任意对象(包括null)
+ * @author sczyh30
+ * @version 0.1
+ * Issue:暂不支持函数式编程接口
+ * 下一版本：争取支持函数式接口
+ */
+public final class Optional<T> {
+
+    private static final Optional<?> EMPTY = new Optional<>();
+
+    private final T object;
+
+    private Optional() {
+        this.object = null;
+    }
+
+    public static <T> Optional<T> get(T object) {
+        return new Optional<>();
+    }
+}
