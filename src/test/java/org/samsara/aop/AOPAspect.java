906a90d2f30180fe66cf99f8949002d88373242b
sczyh30 <sczyh16@gmail.com>
2015-09-02 17:44:08 +0800

    samsara aop updated

--- src/test/java/org/samsara/aop/AOPAspect.java
+++ src/test/java/org/samsara/aop/AOPAspect.java
@@ -8,15 +8,24 @@ import org.samsara.aop.framework.annotation.*;
 @Aspect
 public class AOPAspect {
 
+    /**
+     * The pointcut
+     */
     @Pointcut(value = "all")
     public void doPointcut() {
     }
 
+    /**
+     * The Before Advice
+     */
     @BeforeAdvice(value = "doPointCut")
     public void beforeAdvice() {
         System.out.println("Aspect Test - Before Advice");
     }
 
+    /**
+     * The After Advice
+     */
     @AfterAdvice(value = "doPointCut")
     public void afterAdvice() {
         System.out.println("Aspect Test - After Advice");
